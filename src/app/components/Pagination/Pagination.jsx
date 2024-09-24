b7e52e5f1d614fee4fdac9d6b38d63f007401cdc
Sanjay Malviya <sanjay.profilics@gmail.com>
2024-09-24 18:52:25 +0530

    shopWrapper update using api fetch

--- src/app/components/Pagination/Pagination.jsx
+++ src/app/components/Pagination/Pagination.jsx
@@ -10,22 +10,94 @@ function Pagination({ items, pageSize, currentPage, onPageChange }) {
   const pageCount = Math.ceil(items / pageSize);
   if (pageCount === 1) return null;
   const pages = Array.from({ length: pageCount }, (a, i) => i + 1);
+
+  const startPage = Math.max(1, currentPage - 1);
+  const endPage = Math.min(pageCount, currentPage + 1);
+  const visiblePages = pages.slice(startPage - 1, endPage);
+
   return (
-    <div className="p-10">
+    <div className="pagination-container">
       <ul className="pagination">
-        {pages.map((page) => (
+        <li>
+          <div className="pagination-info">
+            <span>Showing {pageCount} Pages </span>
+            <span>for {items} Products</span>
+          </div>
+        </li>
+        <li
+          className={`pageItem ${currentPage === 1 ? "disabled" : ""}`}
+          onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+        >
+          <a
+            style={{
+              backgroundColor: "#ff0044",
+              padding: "7px 15px",
+              borderRadius: "20px",
+            }}
+          >
+            Previous
+          </a>
+        </li>
+        {visiblePages.map((page) => (
           <li
             key={page}
             className={page === currentPage ? "pageItemActive" : "pageItem"}
+            onClick={() => onPageChange(page)}
           >
-            <a className="pageLink" onClick={() => onPageChange(page)}>
-              {page}
-            </a>
+            <a className="pageLink">{page}</a>
           </li>
         ))}
+        <li
+          className={`pageItem ${currentPage === pageCount ? "disabled" : ""}`}
+          onClick={() =>
+            currentPage < pageCount && onPageChange(currentPage + 1)
+          }
+        >
+          <a
+            style={{
+              backgroundColor: "#ff0044",
+              padding: "7px 15px",
+              borderRadius: "20px",
+            }}
+          >
+            Next
+          </a>
+        </li>
       </ul>
     </div>
   );
 }
 
 export default Pagination;
+
+// import React from "react";
+// import "./pagination.css";
+
+// export const paginate = (items, pageNumber, pageSize) => {
+//   const startIndex = (pageNumber - 1) * pageSize;
+//   return items.slice(startIndex, startIndex + pageSize);
+// };
+
+// function Pagination({ items, pageSize, currentPage, onPageChange }) {
+//   const pageCount = Math.ceil(items / pageSize);
+//   if (pageCount === 1) return null;
+//   const pages = Array.from({ length: pageCount }, (a, i) => i + 1);
+//   return (
+//     <div className="p-10">
+//       <ul className="pagination">
+//         {pages.map((page) => (
+//           <li
+//             key={page}
+//             className={page === currentPage ? "pageItemActive" : "pageItem"}
+//           >
+//             <a className="pageLink" onClick={() => onPageChange(page)}>
+//               {page}
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Pagination;
--- src/app/components/Wrapper/ShopWrapper.jsx
+++ src/app/components/Wrapper/ShopWrapper.jsx
@@ -1,126 +1,143 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
 import Pagination, { paginate } from "../Pagination/Pagination";
-import Categories from "../Categories/Categories";
 
 function ShopWrapper() {
-  const [Products, setProducts] = useState(null);
-  const [ProductsTemp, setProductsTemp] = useState(null);
-  const [paginatedProducts, setPaginatedProducts] = useState(null);
+  const [allProducts, setAllProducts] = useState([]);
+  const [Products, setProducts] = useState([]);
+  const [paginatedProducts, setPaginatedProducts] = useState([]);
   const [category, setCategory] = useState(null);
   const [selectedCategories, setselectedCategories] = useState({});
   const [Brands, setBrands] = useState(null);
+  const [filteredBrands, setFilteredBrands] = useState(null);
   const [HighestPrice, setHighestPrice] = useState(1000);
   const [lowestPrice, setLowestPrice] = useState(0);
   const [selectedBrand, setselectedBrand] = useState({});
-
   const [currentPage, setCurrentPage] = useState(1);
   const pageSize = 6;
-  const onPageChange = (page) => {
-    setCurrentPage(page);
-  };
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
-      .then((data) => {
-        setProducts(data.products);
-        setProductsTemp(data.products);
-        const productsByCategory = data.products.reduce(
-          (arrayCategory, product) => {
-            arrayCategory[product.category] =
-              (arrayCategory[product.category] || 0) + 1;
-            return arrayCategory;
-          },
-          {}
-        );
-        setCategory(productsByCategory);
 
-        const productsByBrand = data.products.reduce((arrayBrand, product) => {
-          if (product.brand != undefined) {
-            arrayBrand[product.brand] = (arrayBrand[product.brand] || 0) + 1;
-          }
-          return arrayBrand;
-        }, {});
-        setBrands(productsByBrand);
-      })
-      .catch((error) => console.error("Error fetching products:", error));
-  }, []);
+  const fetchAllProducts = async () => {
+    try {
+      const response = await fetch(`https://dummyjson.com/products`);
+      const data = await response.json();
+      setAllProducts(data.products);
 
-  useEffect(() => {
-    let categoryArray = [];
-    setProducts(categoryArray);
-    const TempProduct = ProductsTemp;
+      const productsByCategory = data.products.reduce(
+        (arrayCategory, product) => {
+          arrayCategory[product.category] =
+            (arrayCategory[product.category] || 0) + 1;
+          return arrayCategory;
+        },
+        {}
+      );
+      setCategory(productsByCategory);
 
-    Object.keys(selectedCategories).map((category) => {
-      TempProduct.map((product) => {
-        if (product.category == category) {
-          categoryArray.push(product);
-        }
-      });
-    });
-    if (ProductsTemp || categoryArray.length) {
-      const categoryForBrand = categoryArray.length
-        ? categoryArray
-        : ProductsTemp;
-      const productsByBrand = categoryForBrand.reduce((arrayBrand, product) => {
-        if (product.brand != undefined) {
+      const productsByBrand = data.products.reduce((arrayBrand, product) => {
+        if (product.brand !== undefined) {
           arrayBrand[product.brand] = (arrayBrand[product.brand] || 0) + 1;
         }
         return arrayBrand;
       }, {});
       setBrands(productsByBrand);
-    }
-    if (Object.keys(selectedBrand).length) {
-      const tempCategoryArray = [];
-      Object.keys(selectedBrand).map((brand) => {
-        if (categoryArray.length) {
-          categoryArray.map((product) => {
-            if (product.brand == brand) {
-              tempCategoryArray.push(product);
-            }
-          });
-        } else {
-          ProductsTemp.map((product) => {
-            if (product.brand == brand) {
-              tempCategoryArray.push(product);
-            }
-          });
-        }
-      });
-      categoryArray = tempCategoryArray;
-    }
-    function getUniqueListBy(categoryArray, key) {
-      return [
-        ...new Map(categoryArray.map((item) => [item[key], item])).values(),
-      ];
-    }
-    categoryArray = getUniqueListBy(categoryArray, "id");
 
-    let priceArr = [];
-    categoryArray.map((product) => {
-      if (product.price <= HighestPrice && product.price >= lowestPrice) {
-        priceArr.push(product);
+      const highestPrice = Math.max(
+        ...data.products.map((product) => product.price)
+      );
+      const lowestPrice = Math.min(
+        ...data.products.map((product) => product.price)
+      );
+      setHighestPrice(highestPrice);
+      setLowestPrice(lowestPrice);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+  useEffect(() => {
+    let filteredProducts = allProducts;
+    if (Object.keys(selectedCategories).length) {
+      async function getCategories() {
+        const categoriesToFetch = Object.keys(selectedCategories).filter(
+          (category) => selectedCategories[category]
+        );
+        const fetchPromises = categoriesToFetch.map((category) =>
+          fetch(`https://dummyjson.com/products/category/${category}`).then(
+            (res) => res.json()
+          )
+        );
+
+        Promise.all(fetchPromises)
+          .then((results) => {
+            const allProducts = results.flatMap((result) => result.products);
+            setProducts(allProducts);
+            console.log(allProducts);
+            return allProducts;
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
       }
-    });
-
-    categoryArray = priceArr;
-
-    setCurrentPage(1);
-    if (categoryArray && categoryArray.length != 0) {
-      setProducts(categoryArray);
-    } else {
-      setProducts(ProductsTemp);
+      getCategories();
     }
-  }, [selectedCategories, selectedBrand, HighestPrice, lowestPrice]);
+
+    setProducts(filteredProducts);
+    setCurrentPage(1);
+  }, [selectedCategories]);
+  useEffect(() => {
+    let filteredProducts = allProducts;
+    if (Object.keys(selectedBrand).length) {
+      filteredProducts = filteredProducts.filter(
+        (product) => selectedBrand[product.brand]
+      );
+    }
+
+    setProducts(filteredProducts);
+    setCurrentPage(1);
+  }, [selectedBrand]);
+
+  useEffect(() => {
+    let filteredProducts = allProducts;
+
+    filteredProducts = filteredProducts.filter(
+      (product) => product.price <= HighestPrice && product.price >= lowestPrice
+    );
+
+    setProducts(filteredProducts);
+    setCurrentPage(1);
+  }, [HighestPrice, lowestPrice, allProducts]);
 
   useEffect(() => {
     if (Products) {
       const paginatedProducts = paginate(Products, currentPage, pageSize);
       setPaginatedProducts(paginatedProducts);
     }
-  }, [Products, currentPage, ProductsTemp, selectedCategories]);
+  }, [Products, currentPage]);
+
+  useEffect(() => {
+    if (selectedCategories && Object.keys(selectedCategories).length) {
+      const filteredBrands = allProducts
+        .filter((product) => selectedCategories[product.category])
+        .reduce((arrayBrand, product) => {
+          if (product.brand !== undefined) {
+            arrayBrand[product.brand] = (arrayBrand[product.brand] || 0) + 1;
+          }
+          return arrayBrand;
+        }, {});
+      setFilteredBrands(filteredBrands);
+    } else {
+      setFilteredBrands(Brands);
+    }
+  }, [selectedCategories, allProducts, Brands]);
+
+  const onPageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="container grid md:grid-cols-4 grid-cols-2 gap-6 pt-4 pb-16 items-start">
       <div className="col-span-1 bg-white px-4 pb-6 shadow rounded overflow-hiddenb hidden md:block">
@@ -169,13 +186,13 @@ function ShopWrapper() {
               </div>
             </div>
           )}
-          {Brands && (
-            <div className="pt-4">
+          {filteredBrands && (
+            <div className="pt-4" style={{ height: "28rem", overflow: "auto" }}>
               <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">
                 Brands
               </h3>
               <div className="space-y-2">
-                {Object.entries(Brands).map(([key, value], index) => (
+                {Object.entries(filteredBrands).map(([key, value], index) => (
                   <div key={index}>
                     <div className="flex items-center">
                       <input
@@ -247,153 +264,14 @@ function ShopWrapper() {
               />
             </div>
           </div>
-
-          <div className="pt-4">
-            <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">
-              size
-            </h3>
-            <div className="flex items-center gap-2">
-              <div className="size-selector">
-                <input
-                  type="radio"
-                  name="size"
-                  id="size-xs"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="size-xs"
-                  className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600"
-                >
-                  XS
-                </label>
-              </div>
-              <div className="size-selector">
-                <input
-                  type="radio"
-                  name="size"
-                  id="size-sm"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="size-sm"
-                  className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600"
-                >
-                  S
-                </label>
-              </div>
-              <div className="size-selector">
-                <input
-                  type="radio"
-                  name="size"
-                  id="size-m"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="size-m"
-                  className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600"
-                >
-                  M
-                </label>
-              </div>
-              <div className="size-selector">
-                <input
-                  type="radio"
-                  name="size"
-                  id="size-l"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="size-l"
-                  className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600"
-                >
-                  L
-                </label>
-              </div>
-              <div className="size-selector">
-                <input
-                  type="radio"
-                  name="size"
-                  id="size-xl"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="size-xl"
-                  className="text-xs border border-gray-200 rounded-sm h-6 w-6 flex items-center justify-center cursor-pointer shadow-sm text-gray-600"
-                >
-                  XL
-                </label>
-              </div>
-            </div>
-          </div>
-
-          <div className="pt-4">
-            <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">
-              Color
-            </h3>
-            <div className="flex items-center gap-2">
-              <div className="color-selector">
-                <input type="radio" name="color" id="red" className="hidden" />
-                <label
-                  htmlFor="red"
-                  className="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
-                  style={{ backgroundColor: "#fc3d57" }}
-                ></label>
-              </div>
-              <div className="color-selector">
-                <input
-                  type="radio"
-                  name="color"
-                  id="black"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="black"
-                  className="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
-                  style={{ backgroundColor: "#000" }}
-                ></label>
-              </div>
-              <div className="color-selector">
-                <input
-                  type="radio"
-                  name="color"
-                  id="white"
-                  className="hidden"
-                />
-                <label
-                  htmlFor="white"
-                  className="border border-gray-200 rounded-sm h-6 w-6  cursor-pointer shadow-sm block"
-                  style={{ backgroundColor: "#fff" }}
-                ></label>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       {paginatedProducts && (
         <div className="col-span-3">
-          <div className="flex items-center mb-4">
-            <select
-              name="sort"
-              id="sort"
-              className="w-44 text-sm text-gray-600 py-3 px-4 border-gray-300 shadow-sm rounded focus:ring-primary focus:border-primary"
-            >
-              <option value="">Default sorting</option>
-              <option value="price-low-to-high">Price low to high</option>
-              <option value="price-high-to-low">Price high to low</option>
-              <option value="latest">Latest product</option>
-            </select>
-
-            <div className="flex gap-2 ml-auto">
-              <div className="border border-primary w-10 h-9 flex items-center justify-center text-white bg-primary rounded cursor-pointer">
-                <i className="fa-solid fa-grip-vertical"></i>
-              </div>
-              <div className="border border-gray-300 w-10 h-9 flex items-center justify-center text-gray-600 rounded cursor-pointer">
-                <i className="fa-solid fa-list"></i>
-              </div>
-            </div>
-          </div>
-
-          <div className="grid md:grid-cols-3 grid-cols-2 gap-6">
+          <div
+            className="grid md:grid-cols-3 grid-cols-2 gap-6 shadow rounded"
+            style={{ height: "48rem" }}
+          >
             {paginatedProducts.map((product, index) => (
               <div key={index}>
                 <div className="bg-white shadow rounded overflow-hidden group">
@@ -430,7 +308,12 @@ function ShopWrapper() {
                     <Link href="#">
                       <h4
                         className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition"
-                        style={{ height: "50px" }}
+                        style={{
+                          height: "22px",
+                          whiteSpace: "nowrap",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                        }}
                       >
                         {product.title}
                       </h4>
@@ -467,7 +350,7 @@ function ShopWrapper() {
                     </div>
                   </div>
                   <Link
-                    href="#"
+                    href={`/product?id=${product.id}`}
                     className="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition"
                   >
                     Add to cart
